b793f9bcb002415f005d8efbce9e81deb2a22a45
Jorge Montejo <56157795+MontejoJorge@users.noreply.github.com>
2020-12-14 09:54:35 +0100

    download media sh

---
+++ media/downliadMedia.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+rm -rf ./dragontail
+rm -rf ./leagues
+rm -rf ./profileicons
+rm -f ./dragontail.tgz
+rm -f ./leagues.zip
+
+curl -sS http://static.developer.riotgames.com/docs/lol/ranked-emblems.zip > leagues.zip
+
+mkdir leagues
+
+unzip leagues.zip -d ./leagues
+
+clear
+echo "This may take a few minutes..."
+
+curl -sS https://ddragon.leagueoflegends.com/cdn/dragontail-10.25.1.tgz > dragontail.tgz
+
+mkdir dragontail
+
+mkdir profileicons
+
+tar -xzf dragontail.tgz -C ./dragontail
+
+cp ./dragontail/10.25.1/img/profileicon/* ./profileicons > /dev/null 2>&1
+
+rm -rf ./dragontail
+rm ./dragontail.tgz
+rm ./leagues.zip
+
+echo "---------------------DONE---------------------"
