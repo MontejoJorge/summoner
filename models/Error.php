b0a984041d090aa9289d7653b94abe0726b4ac1e
Jorge Montejo <56157795+MontejoJorge@users.noreply.github.com>
2020-12-19 14:03:31 +0100

    add class Error

---
+++ models/Error.php
@@ -0,0 +1,40 @@
+<?php
+
+class LocalError {
+    private $errorIcon;
+
+    private $errorDesc;
+
+    public function __construct(){}
+
+
+
+    /**
+     * Get the value of errorIcon
+     */ 
+    public function getErrorIcon()
+    {
+        return $this->errorIcon;
+    }
+
+    /**
+     * Get the value of errorDesc
+     */ 
+    public function getErrorDesc()
+    {
+        return $this->errorDesc;
+    }
+
+    public function setErrorIcon($errorIcon)
+    {
+        $this->errorIcon = $errorIcon;
+    }
+
+    /**
+     * Get the value of errorDesc
+     */ 
+    public function setErrorDesc($errorDesc)
+    {
+        $this->errorDesc = $errorDesc;
+    }
+}
